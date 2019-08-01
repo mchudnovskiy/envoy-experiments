93ae095679b019b36487ab800fc5b5da08ba5567
Max Chudnovsky <maxchudnovsky@inca-pf18hvyy.sigma.sbrf.ru>
2019-08-01 12:15:31 +0300

    Envoy configuration created

--- cmd/simple_http/simple_http.go
+++ cmd/simple_http/simple_http.go
@@ -7,13 +7,15 @@ import (
 
 func main() {
 	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":7777", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, MakeGreeting(r.URL.Path[1:]))
+	fmt.Printf("\n\n\n Got request. Method: %s \n URL: %s \n Header: %s \n Body: %s", r.Method, r.URL, r.Header, r.Body)
+	result := MakeGreeting(r.URL.Path[1:])
+	fmt.Fprintf(w, result)
 }
 
 func MakeGreeting(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+	return fmt.Sprintf("Hello, %s!\n\n\n\n", name)
 }
